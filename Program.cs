952b2ec8f7b94d5ee1fed19d909da4a18c9f4a76
Justin <justin.halse@gmail.com>
2022-01-09 22:58:21 +0200

    inital

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ScreamingGoat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
